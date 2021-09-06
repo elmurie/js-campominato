161de7d42baee2feda7a59cb50e781da517e845f
elmurie <83522485+elmurie@users.noreply.github.com>
2021-09-06 13:11:49 +0200

    fix timeout function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+/*Esercizio di oggi: Campo Minato
+nome repo: js-campominato
+Consegna
+Il computer deve generare 16 numeri casuali tra 1 e 100 (bombe).
+I numeri non possono essere duplicati.
+In seguito il giocatore clicca sulle celle numerate (non può cliccare più volte sulla stessa cella)
+La partita termina quando il giocatore clicca su un numero “vietato” o clicca su tutte le celle che non sono delle bombe.
+Al termine della partita il software deve comunicare il punteggio.
+BONUS: (da fare solo se funziona tutto il resto)
+all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+con difficoltà 0 => tra 1 e 100
+con difficoltà 1 => tra 1 e 80
+con difficoltà 2 => tra 1 e 50
+*/
+
 /* FUNZIONI */
 
 // Genera numero random
@@ -206,7 +221,7 @@ document.getElementById('campo').addEventListener("click",
         btnAgain.style.display = "inline-block";
         scoreboard.innerHTML = points + (" Punti");
         result.style.display = "block";
-        setTimeout(() => {  result.style.transform = "translate(-50%, -50%)"; }, 1000);
+        setTimeout(() => {  result.style.transform = "translate(-50%, -50%)"; }, 100);
         // Reset variabili
         alreadyClicked = [];
         bombs = [];
